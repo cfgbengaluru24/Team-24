4088657e9608447eed1d1c546e3e02e0877ea4ec
Malay Kumar <malaykumar2003@gmail.com>
2024-07-27 17:34:14 +0530

    Fix: Disable middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,30 +2,30 @@ import { NextResponse, NextRequest } from "next/server";
 import { auth } from "@/auth";
 
 export async function middleware(req: NextRequest) {
-    const session = await auth();
-    const url = req.nextUrl;
-    
-    if (session && (
-        url.toString().startsWith("/signin") ||
-        url.toString().startsWith("/signup") ||
-        url.toString().startsWith("/")
-    )) {
-        console.log("redirecting to /home");
-        return NextResponse.redirect("/home");
-    }
-    if (!session) 
-        return NextResponse.redirect(new URL("/signin", req.url));
+  const session = await auth();
+  const url = req.nextUrl;
 
-    return NextResponse.next();
+  if (
+    session &&
+    (url.toString().startsWith("/signin") ||
+      url.toString().startsWith("/signup") ||
+      url.toString().startsWith("/"))
+  ) {
+    console.log("redirecting to /home");
+    return NextResponse.redirect("/home");
+  }
+  if (!session) return NextResponse.redirect(new URL("/signin", req.url));
+
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: [
-        // '/sign-in',
-        // '/sign-up',
-        '/home',
-        '/about',
-        // '/url',
-        // '/url/:path*',
-    ],
-}
+  matcher: [
+    // '/sign-in',
+    // '/sign-up',
+    // '/home',
+    // '/about',
+    // '/url',
+    // '/url/:path*',
+  ],
+};
