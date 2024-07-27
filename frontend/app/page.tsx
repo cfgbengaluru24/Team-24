75ac90a7fbdfe8fdda84a900a089d10ade3b5dc8
Ankush H V <anshhv2003@gmail.com>
2024-07-28 00:51:46 +0530

    lru update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,3 @@
-import LruSignIn from "@/components/lru/LruSignIn";
 import Image from "next/image";
 
 export default function Home() {
--- frontend/components/lru/LruSignup.tsx
+++ frontend/components/lru/LruSignup.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import { Button } from "../ui/button";
 import {
@@ -20,13 +18,35 @@ import {
 } from "../ui/select";
 import { Input } from "../ui/input";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export function LruSignIn() {
+export function LruSignup() {
   const id = React.useId();
+  const signup = async (formData: FormData) => {
+    "use server"
 
+    const username = formData.get("username") as string
+    const email = formData.get("email") as string
+    const password = formData.get("password") as string
+    const labourers = formData.get("labourers") as string
+    const location = formData.get("location") as string
+
+    if(!username || !email || !password || !labourers || !location)
+      // return new Error("Please fill in all fields")
+      redirect("/signup")
+
+    console.log(username)
+    console.log(email)
+    console.log(password)
+    console.log(labourers)
+    console.log(location)
+    
+    // db logic
+    redirect("/signin")
+}
   return (
     <Card>
-      <form action="">
+      <form action={signup}>
       <CardHeader>
         <CardTitle>Enter the following Details</CardTitle>
         <CardDescription>
@@ -36,20 +56,24 @@ export function LruSignIn() {
       <CardContent className="grid gap-6">
         <div className="grid gap-2">
           <Label htmlFor={`subject-${id}`}>Username:</Label>
-          <Input id={`subject-${id}`} placeholder="Username..." required />
+          <Input id={`subject-${id}`} placeholder="Username..." name="username" required />
+        </div>
+        <div>
+          <Label htmlFor={`email-${id}`}>Email:</Label>
+          <Input id={`email-${id}`} type="email" placeholder="Email..." name="email" required />
         </div>
         <div className="grid gap-2">
           <Label htmlFor={`subject-${id}`}>Password:</Label>
-          <Input id={`subject-${id}`} placeholder="Password..." required />
+          <Input id={`subject-${id}`} placeholder="Password..." name="password" required />
         </div>
         <div className="grid gap-2">
           <Label htmlFor={`subject-${id}`}>Number of Labourers Offered:</Label>
-          <Input id={`subject-${id}`} placeholder="e.g. 25" required />
+          <Input id={`subject-${id}`} placeholder="e.g. 25" name="labourers" required />
         </div>
         <div className="gap-4 sm:grid-cols-2">
           <div className="grid gap-2">
             <Label htmlFor={`area-${id}`}>Location</Label>
-            <Select defaultValue="Reserves">
+            <Select defaultValue="Reserves" name="location">
               <SelectTrigger id={`area-${id}`} aria-label="Area">
                 <SelectValue placeholder="Select" />
               </SelectTrigger>
@@ -71,4 +95,4 @@ export function LruSignIn() {
     </Card>
   );
 }
-export default LruSignIn;
+export default LruSignup;
